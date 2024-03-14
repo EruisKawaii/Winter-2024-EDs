82bce78265c86bfeeeb134ed812d217960b1c3a1
Jan Ming Lam <janlam@uio.no>
2024-03-14 19:21:00 +0100

    aaaa

---
+++ src/js/data/2024-03-14/misc/animeIns.js
@@ -0,0 +1,227 @@
+export const animeInserts = [
+    {
+        name: "Aikatsu on Parade! - \"In bloom\" by Ayane\u00e3\u0192\u00bbRuka\u00e3\u0192\u00bbRie/Karen from BEST FRIENDS!",
+        videoId: "TLZd_WIUC5s",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 1,
+    },
+    {
+        name: "Aldnoah.Zero - \"BRE@TH//LESS\" by Mika Kobayashi",
+        videoId: "rPsmJSrj4i8",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 2,
+    },
+    {
+        name: "Angel Beats! - \"Crow Song\" by Girls Dead Monster (marina)",
+        videoId: "T8bVglOzZ7s",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 3,
+    },
+    {
+        name: "Cyberpunk: Edgerunners - \"I Really Want to Stay at Your House\" by ROSA WALTON (LETS EAT GRANDMA)",
+        videoId: "Rbgw_rduQpM",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 4,
+    },
+    {
+        name: "Detective Conan - \"Kimi ga Ireba\" by Iori",
+        videoId: "s227cAb5i2w",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 5,
+    },
+    {
+        name: "Eyeshield 21 - \"Be Survivor\" by ZZ",
+        videoId: "4x1kivHt-aI",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 6,
+    },
+    {
+        name: "Ga-Rei-Zero - \"Dark Side of the Light\" by Faylan",
+        videoId: "ySwIzlnn928",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 7,
+    },
+    {
+        name: "Tsubasa Chronicle - \"a song of storm and fire\" by Eri Itou",
+        videoId: "jZzpy-IXyxo",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 8,
+    },
+    {
+        name: "Heartcatch Precure! - \"HEART GOES ON\" by Aya Ikeda & Mayu Kudou",
+        videoId: "DeVT2WzxcIY",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 9,
+    },
+    {
+        name: "Id: Invaded - \"Butterfly\" by MIYAVI",
+        videoId: "-N2qioNGR_Y",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 10,
+    },
+    {
+        name: "K-On! - \"Watashi no Koi wa Hotchkiss\" by Ho-Kago Tea Time",
+        videoId: "4KziY05zHeQ",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 11,
+    },
+    {
+        name: "K-On!! - \"U&I\" by Ho-Kago Tea Time",
+        videoId: "nIQko_MvspU",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 12,
+    },
+    {
+        name: "Kill la Kill - \"Sanbika\" by Eir Aoi",
+        videoId: "uo-41UoP_RQ",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 13,
+    },
+    {
+        name: "Love Live! School Idol Project - \"Aishiteru Banzai!\" by Pile",
+        videoId: "2RxLIYVgW6I",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 14,
+    },
+    {
+        name: "Mermaid Melody: Pichi Pichi Pitch - \"Legend of Mermaid ~Piano Version~\" by Kana Ueda",
+        videoId: "MQ-uB83j_yc",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 15,
+    },
+    {
+        name: "One Piece Film: Red - \"Gyakkou\" by Ado",
+        videoId: "vHfAjfKVMew",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 16,
+    },
+    {
+        name: "Promare - \"Kakusei\" by Superfly",
+        videoId: "7u052cW1e1Y",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 17,
+    },
+    {
+        name: "Re:Creators - \"Layers\" by Aimee Blackschleger",
+        videoId: "I-O9yMcUAqE",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 18,
+    },
+    {
+        name: "REDLINE - \"And it's so beautiful\" by James Shimoji feat.Kitty Brown",
+        videoId: "mmlWJlh4ptk",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 19,
+    },
+    {
+        name: "School Days - \"Kanashimi no Mukou e\" by Kanako Itou",
+        videoId: "8ZQ6aZ81_tc",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 20,
+    },
+    {
+        name: "Senki Zesshou Symphogear GX  - \"RADIANT FORCE (IGNITED arrangement)\" by Aoi Yuuki\u00c3\u2014Nana Mizuki\u00c3\u2014Ayahi Takagaki",
+        videoId: "txJBiE1r8Gk",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 21,
+    },
+    {
+        name: "Suzumiya Haruhi no Yuuutsu - \"God knows...\" by Aya Hirano",
+        videoId: "5QNaSMiZ_-o",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 22,
+    },
+    {
+        name: "Violet Evergarden - \"Letter\" by TRUE",
+        videoId: "wwb7WkZ1Slg",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 23,
+    },
+    {
+        name: "Vivy: Fluorite Eye's Song - \"Fluorite Eye's Song\" by Kairi Yagi",
+        videoId: "2qcvp5nE4Lc",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 24,
+    },
+    {
+        name: "Yofukashi no Uta - \"Loss Time\" by Creepy Nuts",
+        videoId: "MauRFgX21Ek",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AnimeIns"]
+        },
+        miyeId: 25,
+    }
+    ]
---
+++ src/js/data/2024-03-14/misc/nonAni.js
@@ -0,0 +1,398 @@
+export const nonAniSongs = [
+    {
+        name: "Routine - Silent Siren",
+        videoId: "MYbRVT4M4ho",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 1,
+    },
+    {
+        name: "Mentai Cosmic (feat. nicamoq) - Yunomi",
+        videoId: "6yZNi4PGyfo",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 2,
+    },
+    {
+        name: "Ano Neko no Hanashi feat. Kubotakai - Miyuna",
+        videoId: "JeTINEDkgqA",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 3,
+    },
+    {
+        name: "Suicide/ Hayaku Yoru He - suisoh",
+        videoId: "OP_llyfMuI0",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 4,
+    },
+    {
+        name: "NEO - SEKAI",
+        videoId: "zw0JHV7nrUg",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 5,
+    },
+    {
+        name: "Doushite - Takase Toya (feat. Emi Noda)",
+        videoId: "Ct-60mD5yjE",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 6,
+    },
+    {
+        name: "ELEVEN -Japanese ver.- - IVE",
+        videoId: "XfEZzUtdINI",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 7,
+    },
+    {
+        name: "Show - Ado",
+        videoId: "pgXpM4l_MwI",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 8,
+    },
+    {
+        name: "Kanade - Sukima Switch",
+        videoId: "J5Z7tIq7bco",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 9,
+    },
+    {
+        name: "Laughter - Official Higedan Dism",
+        videoId: "kff_DXor7jc",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 10,
+    },
+    {
+        name: "Wo Ai Ni - Cody\u30fbLee(\u674e)",
+        videoId: "uwS__WiOYTI",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 11,
+    },
+    {
+        name: "Otoshimae - Polkadot Stingray",
+        videoId: "QxKG2lUfTRE",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 12,
+    },
+    {
+        name: "The Worst - Ren",
+        videoId: "cdKCxYKxOLk",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 13,
+    },
+    {
+        name: "Grace - Fujii Kaze",
+        videoId: "rSYoIuyks8g",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 14,
+    },
+    {
+        name: "Yoru Naderu Menou - Ayase",
+        videoId: "ZLBUxmIWTa0",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 15,
+    },
+    {
+        name: "Stella - Man with a mission",
+        videoId: "AiehyJNDOoY",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 16,
+    },
+    {
+        name: "Blooming in the mud - Wolpis Carter ",
+        videoId: "40dJS_LC6S8",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 17,
+    },
+    {
+        name: "Just a Sunny Day for You - Yorushika",
+        videoId: "-VKIqrvVOpo",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 18,
+    },
+    {
+        name: "Moshimo Bokura Ga Game No Shuyakude - DAOKO",
+        videoId: "lbz2tAyMChU",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 19,
+    },
+    {
+        name: "CQCQ - Kami-sama, I have noticed",
+        videoId: "Ayqq7qSOxV8",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 20,
+    },
+    {
+        name: "Hoshi ni Naru - Islet feat.Isui",
+        videoId: "23Ud5u-Qr3g",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 21,
+    },
+    {
+        name: "Yuki wa Naniiro - Kotoha",
+        videoId: "tSUAeSyPBU8",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 22,
+    },
+    {
+        name: "Aitakute Ima - MISIA",
+        videoId: "MYo5alIaUOk",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 23,
+    },
+    {
+        name: "Aitouka  - Yoishigure feat. Lucia",
+        videoId: "lrWqeuoMY2g",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 24,
+    },
+    {
+        name: "Sakuranbo - Ai Otsuka",
+        videoId: "upODO6OuOOk",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 25,
+    },
+    {
+        name: "Tegami (Haikei J\u016bgo no Kimi e) - Angela Aki",
+        videoId: "erGCAu_hFqM",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 26,
+    },
+    {
+        name: "Hikoutei - King Gnu",
+        videoId: "MSv7NbfbtU8",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 27,
+    },
+    {
+        name: "NISEGAO - Tianyou",
+        videoId: "KEqAxs9wjn8",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 28,
+    },
+    {
+        name: "HAVE A NICE DAY - WORLD ORDER",
+        videoId: "_mkiGMtbrPM",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 29,
+    },
+    {
+        name: "CH4NGE - Giga",
+        videoId: "LbO4pg_LHUI",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 30,
+    },
+    {
+        name: "GHOST PARTY - Shiggy Jr.",
+        videoId: "Rtft9TT67k8",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 31,
+    },
+    {
+        name: "Nonsite - TEARS OF TRAGEDY",
+        videoId: "Xx8GQCDDJLQ",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 32,
+    },
+    {
+        name: "yoiyoi kokon - Reol",
+        videoId: "8IK6eLTNV1k",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 33,
+    },
+    {
+        name: "good night baby - Shian Inubousaki (CV: Rena Hasegawa)",
+        videoId: "1XQJY3V1O24",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 34,
+    },
+    {
+        name: "happy end - backnumber",
+        videoId: "T8y_RsF4TSw",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 35,
+    },
+    {
+        name: "non fiction - ken hirai",
+        videoId: "TmEJ3afKzEY",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 36,
+    },
+    {
+        name: "Ningen Hatsudenjo - BUDDHA BRAND",
+        videoId: "pxsx4027djE",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 37,
+    },
+    {
+        name: "SUNSET - BLU-SWING",
+        videoId: "gcaaefqWgs4",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 38,
+    },
+    {
+        name: "Rekuen - indigo la End",
+        videoId: "DWZqkJBKhwo",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 39,
+    },
+    {
+        name: "saredokijyutushihasaiwohuru  - Lie And A Chameleon",
+        videoId: "GiTLU2I7Cws",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 40,
+    },
+    {
+        name: "Ghost rule - DECO*27",
+        videoId: "KushW6zvazM",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 41,
+    },
+    {
+        name: "Kegare naki Bara Juuji - Ariabl'eyeS",
+        videoId: "apzUMrZ520I",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 42,
+    },
+    {
+        name: "Good bye bye - Ai Tomioka",
+        videoId: "i7kRQsCIvv0",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 43,
+    },
+    {
+        name: "Nashi no Tsubute - Mikansei Monologue",
+        videoId: "CFILadkG1bg",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["NonAniSongs"]
+        },
+        miyeId: 44,
+    }
+]
